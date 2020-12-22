ac85cf10e91405f239f4eefc3c78a84b214b3f3c
Evheniya Pererva <64014702+pererva-ev@users.noreply.github.com>
2020-12-22 13:49:48 +0200

    add test

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+func genarateMatrix() (res [][]int) {
+	length := 4
+	max := 10
+
+	for i := 0; i < length; i++ {
+		tmp := []int{}
+		for j := 0; j < length; j++ {
+			tmp = append(tmp, rand.Intn(max))
+		}
+		res = append(res, tmp)
+	}
+	return
+}
+
+func TestPositiveSlice(t *testing.T) {
+	matrix := genarateMatrix()
+	fmt.Println("Input:")
+	for i := range matrix {
+		fmt.Println(matrix[i])
+	}
+	res := arrayFromMatrix(&matrix)
+	fmt.Printf("\nOutput: %v", res)
+}
